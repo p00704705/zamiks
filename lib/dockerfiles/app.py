40cf0c44e1e2a0add0538da5c25c9fa17599be6f
p00704705 <petermekhail01@gmail.com>
2024-10-23 22:08:39 +1100

    new html files for ecrypt/decrypt

--- lib/dockerfiles/app.py
+++ lib/dockerfiles/app.py
@@ -1,7 +1,8 @@
 from flask import render_template
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from backend.summarize_text import summarize
 from backend.mongdb_connect import store_text
+from backend.encypt_decrypt_text import cryptic
 
 app = Flask(__name__)
 
@@ -42,5 +43,24 @@ def submit():
 def summarise():
     return render_template('summarize.html')
 
+@app.route('/encrydecryp')
+def crypto():
+    return render_template('crypto.html')
+
+@app.route('/encrypt', methods=['POST'])
+def encrypt():
+    data = request.json
+    input_text = data.get('text', '')
+    shifter_value = int(data.get('shifter', 1))  # Get the shifter value, default to 1
+    # Replace this with your actual encryption logic, possibly using shifter_val
+
+    input_text = data.get('text', '')
+    print(input_text)
+    print(shifter_value)
+    # Replace this with your actual encryption logic
+    encrypted_text = cryptic(input_text, 'encrypt', shifter_value)
+    return jsonify({'encrypted_text': encrypted_text})
+
 if __name__ == '__main__':
+          
     app.run(threaded=True,host='0.0.0.0',port=8081,debug=True)
--- lib/dockerfiles/backend/encypt_decrypt_text.py
+++ lib/dockerfiles/backend/encypt_decrypt_text.py
@@ -3,12 +3,14 @@ import os
 import random
 from collections import defaultdict, Counter
 
-def main():
-    message = input("Enter plaintext or ciphertext: ") 
-    process = input("Enter 'encrypt' or 'decrypt': ")
+def cryptic(text, process, shifter):
+    message = text
+    process = process
+    shift = shifter
+    print(text, process, shifter)
     while process not in ('encrypt', 'decrypt'):
         process = input("Invalid process. Enter 'encrypt' or 'decrypt': ")
-    shift = int(input("Shift value (1-366) = "))
+    # shift = int(input("Shift value (1-366) = "))
     while not 1 <= shift <= 366:
         shift = int(input("Invalid value. Enter digit from 1 to 366: "))
     infile = input("Enter filename with extension: ")
